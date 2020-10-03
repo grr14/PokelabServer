6d9776ffb57a42396f79f1f19099e80af9ae4385
grr14 <laoshi@laposte.net>
2020-10-03 20:05:57 +0200

    still trying to make heroku work

--- src/index.js
+++ src/index.js
@@ -23,8 +23,6 @@ const server = new ApolloServer({
 })
 
 // The `listen` method launches a web server.
-server
-  .listen({ port: process.env.APOLLO_SERVER_PORT || 4000 })
-  .then(({ url }) => {
-    console.log(`🚀  Server ready at ${url}`)
-  })
+server.listen({ port: process.env.PORT || 4000 }).then(({ url }) => {
+  console.log(`🚀  Server ready at ${url}`)
+})
